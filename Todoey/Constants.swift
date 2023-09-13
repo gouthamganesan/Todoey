8ee409b661520cfaffb92a895abb6d228bea89ed
Goutham Ganesan <gouthamganesan98@gmail.com>
2023-09-13 22:22:34 +0530

    Add  handle for updating row acording to user interaction

---
+++ Todoey/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Goutham G on 13/09/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct K {
+    static let reusableCellIdentifier: String = "ToDoItemCell"
+}
---
+++ Todoey/TodoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TodoViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK: - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.reusableCellIdentifier, for: indexPath)
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = itemArray[indexPath.row]
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel!.text = itemArray[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
